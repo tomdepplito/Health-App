01f64efab1de362c9c76e8ae87bbd31b304f2536
tomdepplito <tomdepplito@gmail.com>
2014-03-16 16:39:16 -0700

    reset

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -1,5 +1,5 @@
 class HistoriesController < ApplicationController
-  #before_filter :authenticate_user!
+  before_filter :authenticate_user!
 
   def index
    # @history = History.all
--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Health::Application.routes.draw do
   resources :histories
   resources :medicines
 
-  root :to => 'home#index'
+  root :to => 'histories#index'
   #root :to => 'histories#home'
 end
 
