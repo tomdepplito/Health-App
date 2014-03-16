15751cb0c22218327255f6365cc036f8b485f8eb
tomdepplito <tomdepplito@gmail.com>
2014-03-16 16:45:41 -0700

    Fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Health::Application.routes.draw do
 
-  get "medicines/index"
-  get "medicines/new"
-  get "medicines/create"
-  get "histories/index"
-  get "histories/create"
-  get "histories/edit"
-  get "histories/update"
-  get "histories/show"
-  get "histories/home"
-  get "histories/wip"
+  #get "medicines/index"
+  #get "medicines/new"
+  #get "medicines/create"
+  #get "histories/index"
+  #get "histories/create"
+  #get "histories/edit"
+  #get "histories/update"
+  #get "histories/show"
+  #get "histories/home"
+  #get "histories/wip"
   devise_for :users
   resources :histories
   resources :medicines
